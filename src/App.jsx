470fe75079dab6ff2b2dde4a310bdcfd6954fc7f
fiiwebarry <fiiwebarry@yahoo.com>
2023-09-01 15:03:09 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,97 @@
-import { useState } from 'react'
-import './App.css'
+import icon from "/src/assets/Images/task-manager.avif";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  
-
-  const [todolist, setTodoList] =useState([]);
-  const [newTask, setNewTask]=useState("");
-  const [dueDate, setDueDate]=useState("");
 
 
-  const onClickHandler=()=>{
+  const [todolist, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [dueDate, setDueDate] = useState("");
 
-    const task={
-               id: todolist.length === 0 ? 1 : todolist[todolist.length - 1].id + 1, 
-               TaskName:newTask,
-               Datedue:dueDate,
+
+  const onClickHandler = () => {
+    if (!newTask || !dueDate) {
+      return
+    }
+    const task = {
+      id: todolist.length === 0 ? 1 : todolist[todolist.length - 1].id + 1,
+      TaskName: newTask,
+      Datedue: dueDate,
     };
-    
-    setTodoList([...todolist, task])
-   };
 
-  
-   const deleteButton = (id) => {
+    setTodoList([...todolist, task])
+    setNewTask("");
+    setDueDate("");
+  };
+
+
+  const deleteButton = (id) => {
     setTodoList(todolist.filter((task) => task.id !== id));
   };
-  
 
-   
+
+
 
   return (
-    <> 
-    <div className="todolist-tab-header">
-      <div>
-      <h2 className="text-header">TODOLIST</h2>
-      <label htmlfor="task">ADD TASK</label>
+    <>
+      <div className="task-management-tab container" >
+        <div className="image-tab">
+          <img className="icon-tab" src={icon} />
+        </div>
+        <div className="todolist-tab-header">
+          <div>
+            <h2 className="text-header">TASK <span>MANAGEMENT </span>APP</h2>
 
-      <input type="text" id="task" value={newTask} onChange={(e)=>setNewTask(e.target.value)}/>
-      
-      <label htmlfor="">TASK DUE</label>
-      <input type="date" onChange={(e)=>setDueDate(e.target.value)}/>
-      <button onClick={onClickHandler}>ADD TASK</button>
+            <div className='box-layer'>
+              <div className="new-task">
+                <label htmlFor="task">ADD TASK:</label><br /><br />
+
+                <input type="text" id="task" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+              </div>
+
+              <div className="task-due">
+                <label htmlFor="duedate">TASK DUE DATE:</label><br /><br />
+                <input type="date" onChange={(e) => setDueDate(e.target.value)} />
+              </div>
+
+            </div>
+            <div className="btn-tab">
+              <button onClick={onClickHandler}>ADD TASK</button></div>
+
+          </div>
+
+        </div>
+
+      </div><hr />
+
+      <div className="task-layout">
+        <div className='task-tab'>
+          {todolist.map((task) => {
+
+            // const {id,TaskName,Datedue}=task;
+            return (
+              <div className='header-tab' key={task.id}>
+                <h2 className="font"><span>TASK:</span> {task.TaskName}</h2><br />
+
+                <h2 className="font"><span>DUE DATE: </span>{task.Datedue}</h2><br />
+
+
+
+
+
+                <button className="delete-btn" type="button" onClick={() => deleteButton(task.id)}>DELETE TASK</button>
+              </div>)
+
+
+          })}
+        </div>
+
       </div>
-   
-    </div>
-       
-       <div>
-
-      {todolist.map((task)=>{
-
-        // const {id,TaskName,Datedue}=task;
-       return( 
-       <div key={task.id}> 
-        <h2>TASK</h2>
-        <p>{task.TaskName}</p>
-  
-  <h2>DUE DATE</h2>
-   <p>{task.Datedue}</p>
-   <button type="button" onClick={()=>deleteButton(task.id)}>X</button> 
-  </div>)
 
 
-})}  
-       </div>
-       
-        
 
-       
+
 
     </>
   )
